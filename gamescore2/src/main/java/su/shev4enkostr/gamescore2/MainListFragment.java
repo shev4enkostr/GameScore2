31438534d7a5599171fadb6bddddee8ef5c4ffcd
shev4enkostr <shev4enkostr@yahoo.com>
2015-07-14 14:18:49 +0300

    add undo redo

--- gamescore2/src/main/java/su/shev4enkostr/gamescore2/MainListFragment.java
+++ gamescore2/src/main/java/su/shev4enkostr/gamescore2/MainListFragment.java
@@ -25,6 +25,9 @@ public class MainListFragment extends ListFragment implements View.OnClickListen
 
 	private ArrayList<Players> players; // for order to not create new instances of the Players.class always when the Fragment is created
 	private ArrayList<Players> data; // for list adapter
+	private HashMap<Integer, ArrayList<Integer>> historyScore;
+	private ArrayList<Integer> tempHistoryScoreList;
+	private int undoRedoCount = 0;
     private AppListAdapter adapter; // custom adapter
 	
 	private SharedPreferences sharedPref;
@@ -35,6 +38,9 @@ public class MainListFragment extends ListFragment implements View.OnClickListen
 	private static final String ARGUMENT_ADD_DIALOG = "add_dialog";
 	private static final String ARGUMENT_SAVE_PLAYERS = "players_main";
 	private static final String ARGUMENT_SAVE_NUMBER_OF_PLAYER = "number_of_player";
+	private static final String ARGUMENT_SAVE_HISTORY_SCORE = "history_score";
+	private static final String ARGUMENT_SAVE_HISTORY_SCORE_SIZE = "history_score_size";
+	private static final String ARGUMENT_SAVE_UNDO_REDO_COUNT = "undo_redo_count";
 	private static final String ARGUMENT_SAVE_PLAYER_NAME = "player_name";
 	private static final String ARGUMENT_SAVE_PLAYER_SCORE = "player_score";
 	private static final int MIN_SEEK_POSITION = 2;
@@ -59,6 +65,8 @@ public class MainListFragment extends ListFragment implements View.OnClickListen
 		{
 			createPlayers();
 			loadScore();
+			historyScore = new HashMap<>();
+			//getActivity().invalidateOptionsMenu();
 		}
 		else
 			restorePlayers(savedInstanceState);
@@ -71,12 +79,9 @@ public class MainListFragment extends ListFragment implements View.OnClickListen
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
        	Log.d(LOG, "MainListFragment onCreateView()");
-
 		View view = inflater.inflate(R.layout.fragment, null);
-		
 		btnSubmit = (Button) view.findViewById(R.id.btn_submit);
 		btnSubmit.setOnClickListener(this);
-		
 		return view;
     }
 	
@@ -112,6 +117,22 @@ public class MainListFragment extends ListFragment implements View.OnClickListen
 	}
 
 	@Override
+	public void onPrepareOptionsMenu(Menu menu)
+	{
+		if (undoRedoCount <= 0)
+			menu.findItem(R.id.action_undo).setEnabled(false);
+		else
+			menu.findItem(R.id.action_undo).setEnabled(true);
+			
+		if (undoRedoCount >= (historyScore.size() - 1))
+			menu.findItem(R.id.action_redo).setEnabled(false);
+		else
+			menu.findItem(R.id.action_redo).setEnabled(true);
+			
+		super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
 		int id = item.getItemId();
@@ -127,6 +148,16 @@ public class MainListFragment extends ListFragment implements View.OnClickListen
 				
 				// Code without checking max number of players from preference
 				new AddPlayerDialogFragment().show(getFragmentManager(), ARGUMENT_ADD_DIALOG);
+				break;
+			case R.id.action_undo:
+				undoScore();
+				break;
+			case R.id.action_redo:
+				redoScore();
+				break;
+			case R.id.action_clear:
+				clearScore();
+				break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
@@ -137,6 +168,13 @@ public class MainListFragment extends ListFragment implements View.OnClickListen
 		super.onSaveInstanceState(outState);
 		outState.putParcelableArrayList(ARGUMENT_SAVE_PLAYERS, players);
 		outState.putInt(ARGUMENT_SAVE_NUMBER_OF_PLAYER, numberOfPlayer);
+		// Save history
+		outState.putInt(ARGUMENT_SAVE_UNDO_REDO_COUNT, undoRedoCount);
+		outState.putInt(ARGUMENT_SAVE_HISTORY_SCORE_SIZE, historyScore.size());
+		for (int i = 0; i < historyScore.size(); i++)
+		{
+			outState.putIntegerArrayList((ARGUMENT_SAVE_HISTORY_SCORE + i), historyScore.get(i));
+		}
 	}
 	
 	@Override
@@ -203,14 +241,23 @@ public class MainListFragment extends ListFragment implements View.OnClickListen
 		players.trimToSize();
 	}
 
-	// Restore players from savedInstanceState()
+	// Restore players from savedInstanceState
 	public void restorePlayers(Bundle savedInstanceState)
 	{
 		players = savedInstanceState.getParcelableArrayList(ARGUMENT_SAVE_PLAYERS);
 		numberOfPlayer = savedInstanceState.getInt(ARGUMENT_SAVE_NUMBER_OF_PLAYER);
-		
+		// Restore players in list
 		for (int i = 0; i < numberOfPlayer; i++)
 			data.add(i, players.get(i));
+		// Restore history
+		undoRedoCount = savedInstanceState.getInt(ARGUMENT_SAVE_UNDO_REDO_COUNT, 0);
+		historyScore = new HashMap<>();
+		int historyScoreSize = savedInstanceState.getInt(ARGUMENT_SAVE_HISTORY_SCORE_SIZE, 0);
+		for (int i = 0; i < historyScoreSize; i++)
+		{
+			historyScore.put(i, savedInstanceState.getIntegerArrayList(ARGUMENT_SAVE_HISTORY_SCORE + i));
+		}
+		getActivity().invalidateOptionsMenu();
 	}
 	
 	// Restore players from SharedPreference (after then app was killed)
@@ -227,17 +274,25 @@ public class MainListFragment extends ListFragment implements View.OnClickListen
 			Toast.makeText(getActivity(), R.string.toast_no_name, Toast.LENGTH_SHORT).show();
 		else
 		{
+			// Add new player
 			String name = etAddPlayer.getText().toString();
 			players.get(numberOfPlayer).setName(name);
 			data.add(numberOfPlayer, players.get(numberOfPlayer));
 			numberOfPlayer++;
+			// Clear history
+			clearHistory();
 		}
 	}
 	
 	public void addScore()
 	{
+		tempHistoryScoreList = new ArrayList<>();
 		for (int i = 0; i < numberOfPlayer; i++)
 		{
+			// Save score in history
+			tempHistoryScoreList.add(data.get(i).getScore());
+			
+			// Add score
 			View view = getListView().getChildAt(i);
 			EditText etScore = (EditText) view.findViewById(R.id.et_enter_score_player);
 			
@@ -246,7 +301,70 @@ public class MainListFragment extends ListFragment implements View.OnClickListen
 				
 			etScore.setText("");
 		}
+		// Update list
 		adapter.notifyDataSetChanged();
+		// Put history data to HashMap
+		historyScore.put(undoRedoCount, tempHistoryScoreList);
+		undoRedoCount++;
+		// Update menu
+		getActivity().invalidateOptionsMenu();
+	}
+	
+	public void clearScore()
+	{
+		saveScoreInHistory();
+		for (int i = 0; i < adapter.getCount(); i++)
+		{
+			data.get(i).setScore(0);
+		}
+		adapter.notifyDataSetChanged();
+	}
+
+	public void undoScore()
+	{
+		// Save last score
+		saveScoreInHistory();
+		undoRedoCount--;
+		// Undo previous score
+		undoRedoCount--;
+		getScoreFromHistory();
+	}
+
+	public void redoScore()
+	{
+		undoRedoCount++;
+		getScoreFromHistory();
+	}
+	
+	public void saveScoreInHistory()
+	{
+		tempHistoryScoreList = new ArrayList<>();
+		for (int i = 0; i < data.size(); i++)
+		{
+			tempHistoryScoreList.add(data.get(i).getScore());
+		}
+		historyScore.put(undoRedoCount, tempHistoryScoreList);
+		undoRedoCount++;
+		getActivity().invalidateOptionsMenu();
+	}
+	
+	public void getScoreFromHistory()
+	{	
+		tempHistoryScoreList = new ArrayList<>();
+		tempHistoryScoreList = historyScore.get(undoRedoCount);
+		for (int i = 0; i < data.size(); i++)
+		{
+			data.get(i).setScore(tempHistoryScoreList.get(i));
+		}
+		adapter.notifyDataSetChanged();
+		getActivity().invalidateOptionsMenu();
+	}
+	
+	public void clearHistory()
+	{
+		historyScore = new HashMap<>();
+		undoRedoCount = 0;
+		getActivity().invalidateOptionsMenu();
 	}
 	
 	// Before killing app
@@ -307,6 +425,8 @@ public class MainListFragment extends ListFragment implements View.OnClickListen
 			players.add(temp);
 		}
 		players.trimToSize();
+		// Clear history
+		clearHistory();
 	}
 	
 	//Dialog add player class
